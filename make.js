e59bea0960258542d17178501152b28994de3d28
yaskevich <alyaxey.yaskevich@gmail.com>
2017-02-28 16:15:50 +0300

    make.js

--- make.js
+++ make.js
@@ -2,28 +2,24 @@ function add(x, y) { return x + y; }
 function mul(x, y) { return x * y; }
 
 function make(a) { 
-console.log("top",a);
-var arr = [];
-arr.push(a);
-	function fn(...args) {
-		if (args.length){
-			let param = args[0];
-			if (typeof param == "function"){
-				return [...arr].reduce((x,y)=>param(x,y));
-			} else {
-				arr.push(param);
-				return fn.apply(args);
-			}
-			
+	var arr = [a];
+	return function fn(...args) {
+		let param = args[0];
+		if (typeof param === "function"){
+			return [...arr].reduce((x,y)=>param(x,y));
+		} else {
+			arr.push(param);
+			// return fn.apply(args);
+			return fn;
 		}
-		return fn;
 		
-	}
-	return fn;
+		return fn;
+	}	
 }
 
 var s = make(1)(2)(3)(4)(5);
 console.log(s(mul));
+console.log(s(add));
 // Assert(s(add) == 15);
 // Assert(s(mul) == 120);
 // var x = make(5)(10)(15);
