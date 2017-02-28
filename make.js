436f67e01fa87361d7107a1e8b0b95fed6ddf0f9
yaskevich <alyaxey.yaskevich@gmail.com>
2017-02-28 16:12:14 +0300

    make.js

--- make.js
+++ make.js
@@ -1,10 +1,13 @@
 function add(x, y) { return x + y; }
 function mul(x, y) { return x * y; }
-function make(/*???*/) { /* ??? */ }
+function make(a) { 
+
+	
+}
 
 var s = make(1)(2)(3)(4)(5);
-Assert(s(add) == 15);
-Assert(s(mul) == 120);
-var x = make(5)(10)(15);
-Assert(x(add) == 30);
-Assert(x(mul) == 750);
+// Assert(s(add) == 15);
+// Assert(s(mul) == 120);
+// var x = make(5)(10)(15);
+// Assert(x(add) == 30);
+// Assert(x(mul) == 750);
